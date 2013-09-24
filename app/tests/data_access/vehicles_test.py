3b1903e3f7bdc92aa80d8ba4a69c82d65d1e50bf
wanderanimrod <wanderanimrod@gmail.com>
2013-09-24 22:50:18 +0300

    [nim] Refactoring. Cleaning upp

--- app/tests/data_access/vehicles_test.py
+++ app/tests/data_access/vehicles_test.py
@@ -8,16 +8,14 @@ from tests.test_helpers import make_fully_constituted_vehicle
 class VehiclesTest(TestCase):
 
     def setUp(self):
-        db_host_name = settings.host
-        db_port = settings.port
-        self.db = redis.StrictRedis(host=db_host_name, port=db_port, db=0)
+        self.db = redis.StrictRedis(host=settings.host, port=settings.port, db=0)
 
-    def test_should_get_vehicle_by_id_from_redis_hash_map(self):
+    def test_should_get_vehicle_by_id_from_db(self):
         vehicle = self.db_insert_vehicle()
         returned_vehicle = get_vehicle(vehicle.id)
         self.assertEquals(returned_vehicle, vehicle)
 
-    def test_should_return_none_if_vehicle_with_specified_id_does_not_exist_in_the_db(self):
+    def test_get_vehicle_should_return_none_if_vehicle_with_specified_id_does_not_exist_in_the_db(self):
         self.assertIsNone(get_vehicle("NON_EXISTENT_ID"))
 
     def db_insert_vehicle(self, vehicle_id='test_vehicle'):
