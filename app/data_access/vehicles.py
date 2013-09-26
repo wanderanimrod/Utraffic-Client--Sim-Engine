166b3b0cb80e2d654f90a1a5b9b1e66b8cef47b4
wanderanimrod <wanderanimrod@gmail.com>
2013-09-26 10:51:11 +0300

    [nim] Removed unnecessary method that gets one vehicle at a time. We are getting vehicles in batches through redis pipeline for efficient querying

--- app/data_access/vehicles.py
+++ app/data_access/vehicles.py
@@ -4,14 +4,6 @@ from models.vehicle import Vehicle
 db = app.get_db()
 
 
-def get_vehicle(vehicle_id):
-    global db
-    vehicle_dict = db.hgetall(vehicle_id)
-    if vehicle_dict != {}:
-        return Vehicle.make_from_dict(vehicle_dict)
-    return None
-
-
 def get_vehicles(vehicle_ids):
     global db
     pipeline = db.pipeline()
--- app/tests/data_access/vehicles_test.py
+++ app/tests/data_access/vehicles_test.py
@@ -1,8 +1,6 @@
 from unittest import TestCase
 from data_access import settings
-import data_access
-from data_access.vehicles import get_vehicle, get_vehicles
-from mock import MagicMock, Mock
+from data_access.vehicles import get_vehicles
 import redis
 from tests.test_helpers import make_fully_constituted_vehicle
 
@@ -13,21 +11,7 @@ class VehiclesTest(TestCase):
     def setUpClass(cls):
         cls.db = redis.StrictRedis(host=settings.host, port=settings.port, db=0)
 
-    def test_should_get_vehicle_by_id_from_db(self):
-        vehicle = self.db_insert_vehicle()
-        returned_vehicle = get_vehicle(vehicle.id)
-        self.assertEquals(returned_vehicle, vehicle)
-
-    def test_get_vehicle_should_return_none_if_vehicle_with_specified_id_does_not_exist_in_the_db(self):
-        self.assertIsNone(get_vehicle("NON_EXISTENT_ID"))
-
-    def db_insert_vehicle(self, vehicle_id='test_vehicle'):
-        vehicle = make_fully_constituted_vehicle(vehicle_id=vehicle_id)
-        vehicle_dict = vehicle.__dict__
-        self.db.hmset(vehicle_id, vehicle_dict)
-        return vehicle
-
-    def test_get_vehicles_should_get_multiple_vehicles_from_the_db(self):
+    def test_get_vehicles_should_get_multiple_vehicles_from_the_db_by_id(self):
         vehicle_one = make_fully_constituted_vehicle(vehicle_id="vehicle:1")
         vehicle_two = make_fully_constituted_vehicle(vehicle_id="vehicle:2")
         for vehicle in [vehicle_one, vehicle_two]:
@@ -37,3 +21,12 @@ class VehiclesTest(TestCase):
 
     def test_get_vehicles_should_return_empty_list_if_no_vehicles_with_specified_ids_exist_in_the_db(self):
         self.assertEquals(get_vehicles(["NON_EXISTENT_ID_1", "NON_EXISTENT_ID_2"]), [])
+
+    def test_get_vehicles_should_delete_all_returned_vehicles_from_db(self):
+        pass
+
+    def db_insert_vehicle(self, vehicle_id='test_vehicle'):
+        vehicle = make_fully_constituted_vehicle(vehicle_id=vehicle_id)
+        vehicle_dict = vehicle.__dict__
+        self.db.hmset(vehicle_id, vehicle_dict)
+        return vehicle
