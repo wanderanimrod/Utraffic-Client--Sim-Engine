73a368fbfb1c2ce32be9a81560267e584c19b639
wanderanimrod <wanderanimrod@gmail.com>
2013-10-05 16:04:33 +0300

    [nim] Getting vehicle snapshots deletes all the snapshots after fetching them from the db

--- app/data_access/vehicles.py
+++ app/data_access/vehicles.py
@@ -14,4 +14,13 @@ def get_vehicle_snapshots(snapshot_ids):
     for vehicle_dict in vehicles_dict_list:
         if vehicle_dict != {}:
             vehicles.append(VehicleSnapshot.make_from_dict(vehicle_dict))
+    delete_vehicle_snapshots(snapshot_ids)
     return vehicles
+
+
+def delete_vehicle_snapshots(snapshot_ids):
+    global db
+    pipeline = db.pipeline()
+    for snapshot_id in snapshot_ids:
+        pipeline.delete(snapshot_id)
+    pipeline.execute()
--- app/tests/data_access/vehicles_test.py
+++ app/tests/data_access/vehicles_test.py
@@ -12,9 +12,7 @@ class VehiclesTest(TestCase):
         cls.db = redis.StrictRedis(host=settings.host, port=settings.port, db=0)
 
     def test_get_vehicle_snapshots_should_get_multiple_vehicle_snapshots_from_the_db_by_id(self):
-        snapshot_one = make_fully_constituted_vehicle_snapshot(vehicle_id=1, timestamp=0)
-        snapshot_two = make_fully_constituted_vehicle_snapshot(vehicle_id=2, timestamp=0)
-        [self.db_insert_vehicle_snapshot(snapshot) for snapshot in [snapshot_one, snapshot_two]]
+        snapshot_one, snapshot_two = self.insert_two_vehicle_snapshots()
         returned_vehicle_snapshots = get_vehicle_snapshots([snapshot_one.id, snapshot_two.id])
         self.assertEquals(returned_vehicle_snapshots, [snapshot_one, snapshot_two])
 
@@ -22,7 +20,10 @@ class VehiclesTest(TestCase):
         self.assertEquals(get_vehicle_snapshots(["NON_EXISTENT_ID_1", "NON_EXISTENT_ID_2"]), [])
 
     def test_get_vehicles_should_delete_all_returned_vehicles_from_db(self):
-        pass
+        vehicle_snapshots = self.insert_two_vehicle_snapshots()
+        get_vehicle_snapshots(list(vehicle_snapshots))
+        snapshots_left_after_getting_snapshots = get_vehicle_snapshots(list(vehicle_snapshots))
+        self.assertEquals(snapshots_left_after_getting_snapshots, [])
 
     def db_insert_vehicle_snapshot(self, vehicle_snapshot):
         snapshot = make_fully_constituted_vehicle_snapshot(vehicle_id=vehicle_snapshot.vehicle_id,
@@ -30,3 +31,10 @@ class VehiclesTest(TestCase):
         snapshot_dict = snapshot.__dict__
         self.db.hmset(snapshot.id, snapshot_dict)
         return snapshot
+
+    def insert_two_vehicle_snapshots(self):
+        snapshot_one = make_fully_constituted_vehicle_snapshot(vehicle_id=1, timestamp=0)
+        snapshot_two = make_fully_constituted_vehicle_snapshot(vehicle_id=2, timestamp=0)
+        for snapshot in [snapshot_one, snapshot_two]:
+            self.db_insert_vehicle_snapshot(snapshot)
+        return snapshot_one, snapshot_two
