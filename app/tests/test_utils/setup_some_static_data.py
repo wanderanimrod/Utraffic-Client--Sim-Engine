61aa65f72bcaef5898d78dc0429423a668618888
wanderanimrod <wanderanimrod@gmail.com>
2013-11-08 15:42:11 +0300

    [nim] [DEBUG] Adding static data to each series created through the web api and persisting that data across multiple calls to  get_data() on the series

--- app/tests/test_utils/setup_some_static_data.py
+++ app/tests/test_utils/setup_some_static_data.py
@@ -1,15 +1,7 @@
 from models.data_point import DataPoint
-from models.data_server import DataServer
-from models.series import Series
 
 
-def get_data_server():
-    data_server = DataServer()
-    line_graph = Series()
-    graph_id = data_server.add_series(line_graph)
-
+def fill_series_with_static_data(data_server, series_id):
     for i in range(0, 11):
         data_point = DataPoint(i, i)
-        data_server.add_data_point_to_series(data_point, graph_id)
-
-    return data_server
+        data_server.add_data_point_to_series(data_point, series_id)
--- app/web_api/series.py
+++ app/web_api/series.py
@@ -1,5 +1,6 @@
 from flask.ext import restful
 from models.series import Series as SeriesModel
+from tests.test_utils.setup_some_static_data import fill_series_with_static_data
 from web_api.persistent_data_server import get_data_server
 
 
@@ -9,7 +10,12 @@ class OneSeries(restful.Resource):
         data_server = get_data_server()
         try:
             data_points = data_server.get_data_for_series(series_id)
-            return {'dataPoints': data_points, "series": series_id}, 200
+            #Restore the data so we can read it multiple times (overriding designed behaviour)
+            fill_series_with_static_data(data_server, series_id)
+            json_data_points = []
+            for data_point in data_points:
+                json_data_points.append(data_point.json())
+            return {'dataPoints': json_data_points, "series": series_id}, 200
         except Exception, ex:
             return {"error": ex.message}, 404
 
@@ -18,5 +24,8 @@ class Series(restful.Resource):
 
     def post(self):
         data_server = get_data_server()
-        series_id = data_server.add_series(SeriesModel())
+        series = SeriesModel()
+        series_id = data_server.add_series(series)
+        #Take the next line out in prod
+        fill_series_with_static_data(data_server, series.series_id)
         return {'seriesId': series_id}, 201
